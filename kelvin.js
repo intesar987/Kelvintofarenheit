cd33104f6d0364aa88b5346799dbac0139a9dcaa
INTESAR NUR <51456550+intesar987@users.noreply.github.com>
2024-02-12 23:38:21 +0000

    Create kelvin.js

---
+++ kelvin.js
@@ -0,0 +1,10 @@
+const kelvin = 0;
+//kelvin will be 293
+let celsius = kelvin - 273;
+//converts kelvin to celsius
+let farenheit = celsius * (9/5) + 32;
+//converts celsius to farenheit
+farenheit = Math.floor(farenheit)
+// rounds farenheit down
+TEMPERATURE = farenheit
+console.log (`The temperature is ${TEMPERATURE} degrees Farenheit`)
